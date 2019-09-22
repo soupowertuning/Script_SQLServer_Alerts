cb5a2f8c634c8f42cada14123a70c2c2587e671f
Fabricio Lima Soluções em BD <suporte@fabriciolima.net>
2019-09-22 13:46:10 -0300

    Update 3.5 - Deadlock - Job and Alert.sql

--- 3.5 - Deadlock - Job and Alert.sql
+++ 3.5 - Deadlock - Job and Alert.sql
@@ -15,13 +15,12 @@ Linkedin: https://www.linkedin.com/in/fabriciolimasolucoesembd/
 
 Consultoria: comercial@fabriciolima.net
 
-***********************************************************************************************************************************/
 
-/*
 Instructions:
 1 - You need to change the path to a real one of your server
 	Replace: "C:\Temp\Video Alertas"
-*/
+***********************************************************************************************************************************/
+
 
 
 /*
